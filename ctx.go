b3980f1c4fe65e264672e402c80b5c3eb6ff82a8
xiongjianbo <jianbo.xiong@target-energysolutions.com>
2023-09-27 11:20:13 +0800

    add gin context

--- ctx.go
+++ ctx.go
@@ -1,9 +1,188 @@
 package gext
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
-type Context struct {
+type GinServer struct {
+	*gin.Engine
+}
+
+type Gtx struct {
 	*gin.Context
+	ID int
+}
+
+type GinGroup struct {
+	*gin.RouterGroup
+}
+
+func NewGinServer() *GinServer {
+	gServer := &GinServer{Engine: gin.Default()}
+	gServer.Engine.GET(`/ping`, Cors(), func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{`msg`: `pong`})
+	})
+	return gServer
+}
+
+func handleFunc(handler func(c *Gtx)) func(ctx *gin.Context) {
+	return func(c *gin.Context) {
+		handler(&Gtx{Context: c})
+	}
+}
+
+func (server *GinServer) Group(relativePath string, handlers ...func(c *Gtx)) *GinGroup {
+	RHandles := make([]gin.HandlerFunc, 0)
+	for _, handle := range handlers {
+		RHandles = append(RHandles, handleFunc(handle))
+	}
+	return &GinGroup{server.Engine.Group(relativePath, RHandles...)}
+}
+
+func (server *GinServer) GET(relativePath string, handlers ...func(c *Gtx)) gin.IRoutes {
+	RHandles := make([]gin.HandlerFunc, 0)
+	for _, handle := range handlers {
+		RHandles = append(RHandles, handleFunc(handle))
+	}
+	return server.Engine.GET(relativePath, RHandles...)
+}
+
+func (server *GinServer) POST(relativePath string, handlers ...func(c *Gtx)) gin.IRoutes {
+	RHandles := make([]gin.HandlerFunc, 0)
+	for _, handle := range handlers {
+		RHandles = append(RHandles, handleFunc(handle))
+	}
+	return server.Engine.POST(relativePath, RHandles...)
+}
+
+func (server *GinServer) PATCH(relativePath string, handlers ...func(c *Gtx)) gin.IRoutes {
+	RHandles := make([]gin.HandlerFunc, 0)
+	for _, handle := range handlers {
+		RHandles = append(RHandles, handleFunc(handle))
+	}
+	return server.Engine.PATCH(relativePath, RHandles...)
+}
+
+func (server *GinServer) PUT(relativePath string, handlers ...func(c *Gtx)) gin.IRoutes {
+	RHandles := make([]gin.HandlerFunc, 0)
+	for _, handle := range handlers {
+		RHandles = append(RHandles, handleFunc(handle))
+	}
+	return server.Engine.PUT(relativePath, RHandles...)
+}
+
+func (server *GinServer) DELETE(relativePath string, handlers ...func(c *Gtx)) gin.IRoutes {
+	RHandles := make([]gin.HandlerFunc, 0)
+	for _, handle := range handlers {
+		RHandles = append(RHandles, handleFunc(handle))
+	}
+	return server.Engine.DELETE(relativePath, RHandles...)
+}
+
+func (server *GinServer) HEAD(relativePath string, handlers ...func(c *Gtx)) gin.IRoutes {
+	RHandles := make([]gin.HandlerFunc, 0)
+	for _, handle := range handlers {
+		RHandles = append(RHandles, handleFunc(handle))
+	}
+	return server.Engine.HEAD(relativePath, RHandles...)
+}
+
+func (r *GinGroup) GET(relativePath string, handlers ...func(c *Gtx)) gin.IRoutes {
+	rHandles := make([]gin.HandlerFunc, 0)
+	for _, handle := range handlers {
+		rHandles = append(rHandles, handleFunc(handle))
+	}
+	return r.RouterGroup.GET(relativePath, rHandles...)
+}
+
+func (r *GinGroup) POST(relativePath string, handlers ...func(c *Gtx)) gin.IRoutes {
+	rHandles := make([]gin.HandlerFunc, 0)
+	for _, handle := range handlers {
+		rHandles = append(rHandles, handleFunc(handle))
+	}
+	return r.RouterGroup.POST(relativePath, rHandles...)
+}
+
+func (r *GinGroup) PUT(relativePath string, handlers ...func(c *Gtx)) gin.IRoutes {
+	rHandles := make([]gin.HandlerFunc, 0)
+	for _, handle := range handlers {
+		rHandles = append(rHandles, handleFunc(handle))
+	}
+	return r.RouterGroup.PUT(relativePath, rHandles...)
+}
+
+func (r *GinGroup) PATCH(relativePath string, handlers ...func(c *Gtx)) gin.IRoutes {
+	rHandles := make([]gin.HandlerFunc, 0)
+	for _, handle := range handlers {
+		rHandles = append(rHandles, handleFunc(handle))
+	}
+	return r.RouterGroup.PATCH(relativePath, rHandles...)
+}
+
+func (r *GinGroup) DELETE(relativePath string, handlers ...func(c *Gtx)) gin.IRoutes {
+	rHandles := make([]gin.HandlerFunc, 0)
+	for _, handle := range handlers {
+		rHandles = append(rHandles, handleFunc(handle))
+	}
+	return r.RouterGroup.DELETE(relativePath, rHandles...)
+}
+
+func (r *GinGroup) HEAD(relativePath string, handlers ...func(c *Gtx)) gin.IRoutes {
+	rHandles := make([]gin.HandlerFunc, 0)
+	for _, handle := range handlers {
+		rHandles = append(rHandles, handleFunc(handle))
+	}
+	return r.RouterGroup.HEAD(relativePath, rHandles...)
+}
+
+func (r *GinGroup) Use(middlewares ...func(c *Gtx)) gin.IRoutes {
+	rMiddlewares := make([]gin.HandlerFunc, 0)
+	for _, middleware := range middlewares {
+		rMiddlewares = append(rMiddlewares, handleFunc(middleware))
+	}
+	return r.RouterGroup.Use(rMiddlewares...)
+}
+
+func (g *Gtx) JSON(data any) {
+	if g.Request == nil {
+		return
+	}
+	mb, _ := MarshalGzipJson(data)
+	g.Writer.Header().Set("Content-Type", "application/json")
+	g.Writer.Header().Set("Content-Encoding", "gzip")
+	g.Writer.Header().Set("Vary", "Accept-Encoding")
+	g.Writer.Header().Set("Content-Length", fmt.Sprintf("%v", len(mb)))
+	g.Writer.Write(mb)
+	g.Writer.WriteHeader(http.StatusOK)
+	g.Writer.Status()
+}
+
+func (g *Gtx) JSONWithHTTPCode(httpCode int, data any) {
+	if g.Request == nil {
+		return
+	}
+	mb, _ := MarshalGzipJson(data)
+	g.Writer.Header().Set("Content-Type", "application/json")
+	g.Writer.Header().Set("Content-Encoding", "gzip")
+	g.Writer.Header().Set("Vary", "Accept-Encoding")
+	g.Writer.Header().Set("Content-Length", fmt.Sprintf("%v", len(mb)))
+	g.Writer.Write(mb)
+	g.Writer.WriteHeader(httpCode)
+	g.Writer.Status()
+}
+
+type UserInfoKey struct{}
+
+var UserInfoKeyVal = UserInfoKey{}
+
+func GetProfile[U any](c *Gtx) (userInfo *U) {
+	userInfo = new(U)
+	profile := c.Request.Context().Value(UserInfoKeyVal)
+	query, ok := profile.(U)
+	if ok {
+		userInfo = &query
+	}
+	return
 }
--- string.go
+++ string.go
@@ -1,8 +1,11 @@
 package gext
 
 import (
+	"bytes"
+	"compress/gzip"
 	"crypto/rand"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"strconv"
 	"strings"
@@ -81,4 +84,59 @@ func Random(length int) (str string) {
 		str = str[:length]
 	}
 	return
+}
+
+func GzipEncode(input []byte) ([]byte, error) {
+	var buf bytes.Buffer
+	gzipWriter, _ := gzip.NewWriterLevel(&buf, gzip.BestCompression)
+	_, err := gzipWriter.Write(input)
+	if err != nil {
+		_ = gzipWriter.Close()
+		return nil, err
+	}
+	if err = gzipWriter.Close(); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func GzipDecode(input []byte) ([]byte, error) {
+	bytesReader := bytes.NewReader(input)
+	gzipReader, err := gzip.NewReader(bytesReader)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		_ = gzipReader.Close()
+	}()
+	buf := new(bytes.Buffer)
+	if _, err = buf.ReadFrom(gzipReader); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func MarshalGzipJson(data interface{}) ([]byte, error) {
+	marshalData, err := json.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+	gzipData, err := GzipEncode(marshalData)
+	if err != nil {
+		return nil, err
+	}
+	return gzipData, err
+}
+
+func UnmarshalGzipJson(input []byte, output interface{}) error {
+	decodeData, err := GzipDecode(input)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(decodeData, output)
+	if err != nil {
+		return err
+	}
+	return nil
 }
