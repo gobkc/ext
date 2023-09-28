77035d3662629470fe7257ba975bbb21d6c2788c
xiongjianbo <jianbo.xiong@target-energysolutions.com>
2023-09-28 16:13:46 +0800

    add md5 function

--- string.go
+++ string.go
@@ -3,10 +3,12 @@ package gext
 import (
 	"bytes"
 	"compress/gzip"
+	"crypto/md5"
 	"crypto/rand"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"io"
 	"strconv"
 	"strings"
 )
@@ -139,4 +141,11 @@ func UnmarshalGzipJson(input []byte, output interface{}) error {
 		return err
 	}
 	return nil
+}
+
+func Md5(str string) string {
+	w := md5.New()
+	io.WriteString(w, str)
+	md5str := fmt.Sprintf("%x", w.Sum(nil))
+	return md5str
 }
